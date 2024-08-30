7d62547ed73f62404c42393aaba240876b783130
FerushT <ferush82@gmail.com>
2024-08-30 16:25:08 +0200

    add comments

--- lib/src/features/feature1/presentation/weather_screen.dart
+++ lib/src/features/feature1/presentation/weather_screen.dart
@@ -1,86 +1,85 @@
-import 'package:flutter/material.dart'; // Basisbibliothek für UI-Elemente und Material-Design.
-import 'package:provider/provider.dart'; // Bibliothek zur Verwaltung des App-States.
-import 'package:shared_preferences/shared_preferences.dart'; // Bibliothek zum Speichern von Daten auf dem Gerät.
-import 'package:intl/intl.dart'; // Bibliothek zur Formatierung von Datum und Uhrzeit.
-import 'package:weather_app/src/features/feature1/domain/weather_animation.dart'; // Korrekte Import-Anweisung für WeatherAnimation
-import '../domain/weather_provider.dart'; // Import der Wetterlogik (WeatherProvider)
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_app/src/features/feature1/domain/weather_animation.dart';
+import '../domain/weather_provider.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
   @override
   // ignore: library_private_types_in_public_api
-  _WeatherScreenState createState() =>
-      _WeatherScreenState(); // Erzeugt den Zustand (State) für den Bildschirm.
+  _WeatherScreenState createState() => _WeatherScreenState();
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  String cityName =
-      ""; // Variable, um den eingegebenen Stadtnamen zu speichern.
+  String cityName = "";
+  final TextEditingController _textController = TextEditingController();
 
   @override
   void initState() {
-    super
-        .initState(); // Führt die Standardinitialisierungen der übergeordneten Klasse aus.
-    _loadCityName(); // Lädt den gespeicherten Stadtnamen, wenn der Bildschirm gestartet wird.
+    super.initState();
+    _loadCityName();
   }
 
   void _loadCityName() async {
-    final prefs = await SharedPreferences
-        .getInstance(); // Holt sich eine Instanz von SharedPreferences.
-    final savedCityName =
-        prefs.getString('cityName'); // Liest den gespeicherten Stadtnamen.
+    final prefs = await SharedPreferences.getInstance();
+    final savedCityName = prefs.getString('cityName');
     if (savedCityName != null && savedCityName.isNotEmpty) {
       setState(() {
-        cityName = savedCityName; // Setzt den geladenen Stadtnamen.
+        cityName = savedCityName;
+        _textController.text = savedCityName;
       });
-      Provider.of<WeatherProvider>(context, listen: false).fetchWeather(
-          savedCityName); // Holt das Wetter für die gespeicherte Stadt.
+      Provider.of<WeatherProvider>(context, listen: false)
+          .fetchWeather(savedCityName);
     }
   }
 
   void _saveCityName(String cityName) async {
-    final prefs = await SharedPreferences
-        .getInstance(); // Holt sich eine Instanz von SharedPreferences.
-    prefs.setString('cityName', cityName); // Speichert den Stadtnamen.
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('cityName', cityName);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar:
-          true, // Erlaubt es dem Inhalt, hinter der App-Leiste (AppBar) zu erscheinen.
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor:
-            Colors.transparent, // Macht die App-Leiste durchsichtig.
-        elevation: 0, // Entfernt den Schatten der App-Leiste.
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         title: const Text(
-          "Wetter", // Titel der App-Leiste.
+          "Wetter",
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
         bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(
-              100.0), // Höhe des Eingabefelds unter der App-Leiste.
+          preferredSize: const Size.fromHeight(100.0),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: _textController,
               onChanged: (value) {
-                cityName =
-                    value; // Aktualisiert den Stadtnamen, während der Benutzer tippt.
+                cityName = value;
               },
               onSubmitted: (value) async {
                 if (value.isNotEmpty) {
-                  _saveCityName(
-                      value); // Speichert den eingegebenen Stadtnamen.
+                  _saveCityName(value);
                   Provider.of<WeatherProvider>(context, listen: false)
-                      .fetchWeather(
-                          value); // Holt das Wetter für die eingegebene Stadt.
+                      .fetchWeather(value);
                 }
               },
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 hintText: "Ort eingeben",
                 border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {
+                    _textController.clear(); // Löscht den Text im Textfeld
+                    setState(() {
+                      cityName = ""; // Setzt die cityName-Variable zurück
+                    });
+                  },
+                ),
               ),
             ),
           ),
@@ -90,18 +89,16 @@ class _WeatherScreenState extends State<WeatherScreen> {
         children: [
           const SizedBox.expand(
             child: WeatherAnimation(),
-          ), // Animations Widget für den Screen.
+          ),
           Consumer<WeatherProvider>(
             builder: (context, weatherProvider, child) {
               if (weatherProvider.isLoading) {
-                // Zeigt einen Ladeindikator an, wenn Daten geladen werden.
                 return const Center(child: CircularProgressIndicator());
               } else if (weatherProvider.weatherData == null) {
-                // Wenn keine Wetterdaten verfügbar sind
                 return const Center(child: Text("Geben Sie einen Ort ein"));
               } else {
                 final iconUrl =
-                    "http://openweathermap.org/img/wn/${weatherProvider.weatherData!.icon}@2x.png"; // URL für das Wetter-Icon.
+                    "http://openweathermap.org/img/wn/${weatherProvider.weatherData!.icon}@2x.png";
 
                 return Center(
                   child: Column(
@@ -111,7 +108,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                         padding: EdgeInsets.only(top: 70, bottom: 100),
                       ),
                       Image.network(
-                        iconUrl, // Wetter-Icon wird aus dem Internet geladen.
+                        iconUrl,
                         width: 100,
                         height: 100,
                       ),
@@ -141,22 +138,21 @@ class _WeatherScreenState extends State<WeatherScreen> {
                         children:
                             weatherProvider.weatherForecast.map((forecast) {
                           final forecastIconUrl =
-                              "http://openweathermap.org/img/wn/${forecast.icon}@2x.png"; // URL für das Vorhersage-Icon.
+                              "http://openweathermap.org/img/wn/${forecast.icon}@2x.png";
                           return ListTile(
                             leading: Image.network(
-                              forecastIconUrl, // Vorhersage-Icon.
+                              forecastIconUrl,
                               width: 50,
                               height: 50,
                             ),
                             title: Text(
-                              "${forecast.temperature}°C - ${forecast.description}", // Temperatur und Beschreibung der Vorhersage.
+                              "${forecast.temperature}°C - ${forecast.description}",
                             ),
                             subtitle: Text(
-                              DateFormat('dd-MM-yyyy').format(
-                                  forecast.date), // Datum der Vorhersage.
+                              DateFormat('dd-MM-yyyy').format(forecast.date),
                             ),
                           );
-                        }).toList(), // Wandelt die Vorhersagen in eine Liste von Widgets um.
+                        }).toList(),
                       ),
                     ],
                   ),
@@ -169,9 +165,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (cityName.isNotEmpty) {
-            // Überprüft, ob ein Stadtname eingegeben wurde.
-            Provider.of<WeatherProvider>(context, listen: false).fetchWeather(
-                cityName); // Aktualisiert das Wetter für die eingegebene Stadt.
+            Provider.of<WeatherProvider>(context, listen: false)
+                .fetchWeather(cityName);
           }
         },
         backgroundColor: Color.fromARGB(255, 19, 176, 255),
